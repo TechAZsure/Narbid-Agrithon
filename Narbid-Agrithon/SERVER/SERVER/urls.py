31988c2f951dbbc0e42032d708d5b4980c8ca0d8
TechAZsure <148707039+TechAZsure@users.noreply.github.com>
2024-08-16 05:36:38 +0530

    Delete Narbid-Agrithon directory

--- Narbid-Agrithon/SERVER/SERVER/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from display.views import fetch_data
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('display/', fetch_data, name='fetch_data'),
-]
